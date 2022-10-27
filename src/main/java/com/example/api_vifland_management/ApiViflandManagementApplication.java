60af9a9dd226834a726b73ce10977928e88d5f11
LAPTOP-T92SSGCT\Bui Tuan <tuanba.tgl@gmail.com>
2022-10-27 15:49:44 +0700

    tuanBA

---
+++ src/main/java/com/example/api_vifland_management/ApiViflandManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_vifland_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiViflandManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiViflandManagementApplication.class, args);
+    }
+
+}
